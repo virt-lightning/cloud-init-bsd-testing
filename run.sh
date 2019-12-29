310100662c38f48b275e0420ff7a8d1849e6728a
Gonéri Le Bouder <goneri@lebouder.net>
2019-12-29 18:01:15 -0500

    minor

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eux
 export VIRTUAL_ENV_DISABLE_PROMPT=1
-. ~/virt-lightning/bin/activate
+. ~/virtualenv/bin/activate
 
 for i in 8; do
     ansible-playbook playbook.yml -i inventory.yaml -e @targets/netbsd-${i}.yaml
@@ -11,7 +11,7 @@ for i in 11 12; do
     ansible-playbook playbook.yml -i inventory.yaml -e @targets/freebsd-${i}.yaml
 done
 
-for i in 6.6; do
+for i in 6; do
     ansible-playbook playbook.yml -i inventory.yaml -e @targets/openbsd-${i}.yaml
 done
 
