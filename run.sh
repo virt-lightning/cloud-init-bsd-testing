45cc52a19aaea4c4e51a1f5357a5ef3f243ec589
Gonéri Le Bouder <goneri@lebouder.net>
2020-02-24 14:16:36 -0500

    use master branch from goneri/cloud-init

--- run.sh
+++ run.sh
@@ -4,14 +4,17 @@ export VIRTUAL_ENV_DISABLE_PROMPT=1
 . ~/virtualenv/bin/activate
 
 for i in 8; do
-    ansible-playbook playbook.yml -i inventory.yaml -e @targets/netbsd-${i}.yaml
+    ansible-playbook playbook.yml -i inventory.yaml -e @targets/netbsd-${i}.yaml -e promote_image=true
+    ./test-openstack netbsd-${i}
 done
 
 for i in 11 12; do
-    ansible-playbook playbook.yml -i inventory.yaml -e @targets/freebsd-${i}.yaml
+    ansible-playbook playbook.yml -i inventory.yaml -e @targets/freebsd-${i}.yaml -e promote_image=true
+    ./test-openstack freebsd-${i}
 done
 
 for i in 6; do
-    ansible-playbook playbook.yml -i inventory.yaml -e @targets/openbsd-${i}.yaml
+    ansible-playbook -vvv playbook.yml -i inventory.yaml -e @targets/openbsd-${i}.yaml -e promote_image=true
+    ./test-openstack openbsd-${i}
 done
 
