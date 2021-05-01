eade0a3f5ff6d61f39cc1128db7c3309a9871820
Gonéri Le Bouder <goneri@lebouder.net>
2021-05-01 17:48:59 -0400

    update

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-set -eux
+set -euxo pipefail
 export ANSIBLE_SSH_RETRIES=10
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 . ~/virtualenv/bin/activate
 
 
-repo="goneri/cloud-init"
+#repo="goneri/cloud-init"
+repo="canonical/cloud-init"
 promote_image=false
 
 
@@ -13,9 +14,11 @@ log_dir=$(date +results/%Y%m%d-%H%M)
 mkdir -p ${log_dir}
 
 function run_test() {
-    os=$1
-    version=$2
-    git_repo=${3:-canonical/cloud-init}
+    os=${1}
+    version=${2}
+    #git_repo=${3:-canonical/cloud-init}
+    git_repo=${3}
+    vl fetch ${os}-${version}
     ansible-playbook cleanup.yaml
     ansible-playbook playbook.yml -i inventory.yaml -e @targets/${os}-${version}.yaml -e git_repo=${git_repo} -vvv
     timeout 1800 ansible-playbook openstack.yaml -e @targets/${os}-${version}.yaml -vvv
@@ -24,18 +27,18 @@ function run_test() {
 
 }
 
+os=openbsd
+for version in 6.8; do
+    run_test ${os} ${version} ${repo} > ${log_dir}/${os}-${version}-build.log 2>&1
+done
+
 os=freebsd
-for version in 11.4 12.1; do
+for version in 11.4 12.2; do
     run_test ${os} ${version} ${repo} > ${log_dir}/${os}-${version}-build.log 2>&1
 done
 
 os=netbsd
 for version in 8.2 9.1; do
-    run_test ${os} ${version} ${repo} > ${log_dir}/${os}-${version}-build.log 2>&1
-done
-
-os=openbsd
-for version in 6.8; do
     run_test ${os} ${version} ${repo} > ${log_dir}/${os}-${version}-build.log 2>&1
 done
 
